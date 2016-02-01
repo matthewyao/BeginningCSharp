9bfcc5c823e8479aecf2cb034a9a1c17d249c25b
unknown <matthewyao@ITA-1405-0282.synacast.local>
2016-02-01 13:58:47 +0800

    first commit

---
+++ RoleIdentity/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(RoleIdentity.Startup))]
+namespace RoleIdentity
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
